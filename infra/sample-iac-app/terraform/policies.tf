62ce558a5b3ca65c8386182fdd9dbe50ceb7faea
Bill Froemming <williambfroemming@gmail.com>
2025-06-15 14:21:12 -0700

    update alice and bob policies for testing

--- infra/sample-iac-app/terraform/policies.tf
+++ infra/sample-iac-app/terraform/policies.tf
@@ -6,13 +6,27 @@ resource "aws_iam_user_policy" "alice_analyst_policy" {
     Version = "2012-10-17",
     Statement = [
       {
+        Sid      = "OverlyPermissiveReadAndWrite",
         Effect   = "Allow",
-        Action   = "s3:*",
+        Action   = [
+          "s3:*",                         # Full S3 access
+          "athena:*",                     # All Athena actions
+          "glue:*",                       # All Glue actions (overkill for most analysts)
+          "cloudwatch:Get*",              # OK
+          "cloudwatch:PutMetricData",     # Write perms analysts shouldn't need
+          "dynamodb:Scan",                # Too broad for sensitive data
+          "kms:Decrypt",                  # Dangerous without restrictions
+          "iam:List*",                    # Allows recon
+          "iam:Get*",                     # More recon
+          "lambda:InvokeFunction",        # Could be misused
+          "sts:AssumeRole"                # Very risky unless scoped tightly
+        ],
         Resource = "*"
       }
     ]
   })
 }
+
 
 resource "aws_iam_user_policy" "bob_dev_policy" {
   name = "bob-dev-test-policy"
@@ -22,25 +36,64 @@ resource "aws_iam_user_policy" "bob_dev_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = "lambda:*",
-        Resource = "*"
+        Sid: "LambdaOverreach",
+        Effect: "Allow",
+        Action: [
+          "lambda:*"                          # Too much
+        ],
+        Resource: "*"
       },
       {
-        Effect = "Allow",
-        Action = [
+        Sid: "S3FullBucketAccess",
+        Effect: "Allow",
+        Action: [
           "s3:PutObject",
           "s3:GetObject",
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "s3:DeleteObject",                  # Excessive
+          "s3:PutBucketPolicy",               # Definitely too much
+          "s3:GetBucketAcl"
         ],
-        Resource = [
+        Resource: [
           "arn:aws:s3:::ucb-capstone-bucket",
           "arn:aws:s3:::ucb-capstone-bucket/*"
         ]
+      },
+      {
+        Sid: "IAMReconAccess",
+        Effect: "Allow",
+        Action: [
+          "iam:GetRole",
+          "iam:ListRoles"
+        ],
+        Resource: "*"
+      },
+      {
+        Sid: "CloudWatchLogsAccess",
+        Effect: "Allow",
+        Action: [
+          "logs:DescribeLogGroups",
+          "logs:GetLogEvents",
+          "logs:FilterLogEvents",
+          "logs:PutLogEvents"                # Not always needed
+        ],
+        Resource: "*"
+      },
+      {
+        Sid: "ECSAndECRAccess",
+        Effect: "Allow",
+        Action: [
+          "ecs:ListClusters",
+          "ecs:DescribeTasks",
+          "ecr:GetAuthorizationToken",
+          "ecr:DescribeRepositories"
+        ],
+        Resource: "*"
       }
     ]
   })
 }
+
 
 resource "aws_iam_user_policy_attachment" "charlie_admin_access" {
   user       = aws_iam_user.charlie_admin_test.name
